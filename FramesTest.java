cc5ab1d581ad9734e5413f0065cd2596cc6868a4
PRAAVUS <73282565+PRAAVUS@users.noreply.github.com>
2020-10-30 17:29:17 +0100

    Add files via upload

--- FramesTest.java
+++ FramesTest.java
@@ -1,5 +1,6 @@
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,8 +10,9 @@ public class FramesTest {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\kalfj\\eclipse\\java-2020-09\\eclipse\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-		driver.get("https://the-internet.herokuapp.com");	
-		
+		driver.get("https://jqueryui.com/droppable");	
+		driver.switchTo().frame(driver.findElement(By.cssSelector("iframe[class='demo-frame']")));
+		driver.findElement(By.id("draggable")).click();
 		
 		
 		
