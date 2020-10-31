9a954045f1f61bc656409e4359235deac2d2d33d
PRAAVUS <73282565+PRAAVUS@users.noreply.github.com>
2020-10-31 16:01:39 +0100

    Add files via upload

--- Scope.java
+++ Scope.java
@@ -1,5 +1,6 @@
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -18,5 +19,16 @@ public class Scope {
 		//Find the number of links in footer section
 		WebElement footerdriver= driver.findElement(By.id("gf-BIG"));
 		System.out.println("Number of Links in footer: "+footerdriver.findElements(By.tagName("a")).size());
+		
+		//Find the number of links in the first column "Discount Coupon
+		WebElement discountcoupon= footerdriver.findElement(By.xpath("//tbody/tr[1]/td[1]/ul[1]"));
+		System.out.println("Number of Links in discountcoupons: "+discountcoupon.findElements(By.tagName("a")).size());
+		
+		//Open all links in the footer section
+		for(int i=0;i<footerdriver.findElements(By.tagName("a")).size();i++)
+		{
+			String clickonlinktab= Keys.chord(Keys.CONTROL,Keys.ENTER);
+			footerdriver.findElements(By.tagName("a")).get(i).sendKeys(clickonlinktab);
+		}
 	}}
 
