f87647e886a571954561a64653619f3ba5ea53f7
PRAAVUS <73282565+PRAAVUS@users.noreply.github.com>
2020-10-31 11:58:51 +0100

    Add files via upload

--- SwitchingTabs.java
+++ SwitchingTabs.java
@@ -14,12 +14,16 @@ public class SwitchingTabs {
 		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 		driver.get("https://accounts.google.com/signup");	
 		driver.findElement(By.cssSelector("body.EIlDfe:nth-child(2) div.uc81Ff.wKBl8c.bbRbR.RELBvb:nth-child(1) div.LJtPoc.qmmlRd footer.RwBngc ul.Bgzgmd li:nth-child(2) > a:nth-child(1)")).click();
+		
 		System.out.println("Before Switching");
 		System.out.println(driver.getTitle());
+		
 		Set<String>ids=driver.getWindowHandles();
 		Iterator<String> it=ids.iterator();
+		
 		String parentid= it.next();
 		String childid= it.next();
+		
 		driver.switchTo().window(childid);
 		System.out.println("After Switching");
 		System.out.println(driver.getTitle());
